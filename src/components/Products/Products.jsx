90bab5b3458c6015eafb01e7613ae8af84580b2a
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-05-08 20:56:15 +0200

    print a products on DOM

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,36 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Products = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get('http://localhost:8080/products/getAll')
-        console.log('res', res.data.products)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    fetchData()
-  }, [])
-  
-  return (
-    <div>Products</div>
-  )
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const getAllProducts = async () => {
+            try {
+                const res = await axios.get('http://localhost:8080/products/getAll')
+                setProducts(res.data.products)
+                console.log('res', res.data.products)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getAllProducts()
+    }, [])
+
+    return (
+        <div>{products.map( (product) => {
+            return(
+                <div key={product.id}>
+                        <img src="{product.image}" alt="imagen" />
+                    <div>
+                        <h4>{product.name}</h4>
+                        {/* <p>{product.description}</p> */}
+                        <span>{product.price}</span>
+                        <span>{product.Category.category}</span>
+                    </div>
+                </div>
+            )
+        })}</div>
+    )
 }
 
 export default Products
