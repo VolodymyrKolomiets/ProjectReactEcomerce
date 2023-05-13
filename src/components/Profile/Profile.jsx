4431d59058ad614de226ebb2232837a4e9e1beb0
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-05-13 22:07:16 +0200

    register update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,50 +1,33 @@
-import React, { useContext, useEffect } from 'react'
-import { UserContext } from '../../context/UserContext/UserState'
-import { Card, Spin, Collapse } from "antd";
+import React, { useContext, useEffect } from "react";
+import { UserContext } from "../../context/UserContext/UserState";
+import { Card, Spin } from "antd";
 
 const Profile = () => {
-  const { getUserInfo, user } = useContext(UserContext)
+  const { getUserInfo,user } = useContext(UserContext);
   useEffect(() => {
-    getUserInfo()
-  }, [])
-  if (!user) {
-    return <Spin size="large" />;
+    getUserInfo();
+  }, []);
+  if(!user){
+    return   <Spin size="large" />
   }
-  return (
-    <div>
-      <Card
-        title="Profile"
-        extra={<a href="#">{user.role}</a>}
-        style={{
-          width: 300,
-        }}
-      >
-        <img
-          style={{
-            width: 30,
-          }}
-          src="https://img.freepik.com/free-icon/user_318-563642.jpg?w=360"
-          alt=""
-        />
-        <p>Name: {user.name}</p>
-        <p>Age: {user.age}</p>
-        <p>Email: {user.email}</p>
-      </Card>
-      <h2>Orders</h2>
-      {user.orderIds.map((order) => {
-        return (
-          <Collapse defaultActiveKey={["1"]}>
-            <Panel header={order.createdAt} key="1">
-              {order.productIds.map((product) => (
-                <p>{product.name} {product.price} €</p>
-              ))}
-            </Panel>
-          </Collapse>
-        );
-      })}
+  return <div>
+    <Card
+      title="Profile"
+      extra={<a href="#">{user.role}</a>}
+      style={{
+        width: 300,
+      }}
+    >
+        <img  style={{
+        width: 30,
+      }} src="https://img.freepik.com/free-icon/user_318-563642.jpg?w=360" alt="" />
+      <p>Name: {user.name}</p>
+      <p>Surname: {user.surname}</p>
+      <p>Email: {user.email}</p>
 
-    </div>
-  );
+    </Card>
+
+  </div>;
 };
 
 export default Profile
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,82 +1,118 @@
-import React, { useContext, useState } from "react";
-import { UserContext } from "../../context/UserContext/UserState";
+import React, { useState } from "react";
+import "./Register.scss";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  
-  const { postUser, users } = useContext(UserContext);
-  
-  const initialState = {
+  const initialValue = {
     name: "",
-    surname:"",
+    lastName: "",
     email: "",
     password: "",
   };
-
-  const [data, setData] = useState(initialState);
+  const [data, setData] = useState(initialValue);
   const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("");
 
-  const clearState = () => {
-    setData({ ...initialState });
-  };
+
+ const navigate = useNavigate();
 
   const handleInputChange = (event) => {
-    if (data.name.length + 1 < 3) {
-      setMessage("Name must be at least 3 characters");
-
+    if (
+      data.name.length + 1 < 3 ||
+      data.lastName.length + 1 < 3 ||
+      data.password.length === 0
+    ) {
+      setMessage("Name, Last Name, and password are required");
       setBtnDisabled(true);
     } else {
       setMessage(null);
-
       setBtnDisabled(false);
     }
-    setData({
-      ...data,
-      [event.target.name]: event.target.value,
-    });
+    setData({ ...data, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    postUser(data)
-    clearState();
-  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const userInfo = {
+      name: data.name,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password,
+    };
+    let userList = [];
+    try {
+      const existingUsers = localStorage.getItem("userList");
+      if (existingUsers) {
+        userList = JSON.parse(existingUsers);
+      }
+    } catch (error) {
+      console.log("Error loading user list from local storage:", error);
+    }
+  
+    // Verificar si el usuario ya existe en la lista
+    const existingUser = userList.find(user => user.email === userInfo.email);
+    if (existingUser) {
+      alert('Este usuario ya existe en la lista');
+      return;
+    }
+  
+    userList.push(userInfo);
+    localStorage.setItem("userList", JSON.stringify(userList));
+    console.log(`Welcome ${data.name} ${data.lastName}!`);
+    setData(initialValue);
+    navigate("/");
+  }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="name"
-          onChange={handleInputChange}
-          name="name"
-        />
-     
-        <input
-          type="text"
-          placeholder="surname"
-          onChange={handleInputChange}
-          name="surname"
-        />
-
-        <input
-          type="email"
-          placeholder="email"
-          onChange={handleInputChange}
-          name="email"
-        />
-
-<input
-          type="password"
-          placeholder="password"
-          onChange={handleInputChange}
-          name="password"
-        />
-        <button type="submit" disabled={btnDisabled}>
-          Create User
-        </button>
-      </form>
-      {message}
+    <div className="container-form">
+      <div className="form-container">
+        <h1 className="text-form">Register</h1>
+        <form onSubmit={onSubmit}>
+          <div className="input-container">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              onChange={handleInputChange}
+              name="name"
+              value={data.name}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              placeholder="Enter your last name"
+              onChange={handleInputChange}
+              name="lastName"
+              value={data.lastName}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              onChange={handleInputChange}
+              name="email"
+              value={data.email}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="birthdate">Password</label>
+            <input
+              type="text"
+              onChange={handleInputChange}
+              name="password"
+              value={data.password}
+            />
+          </div>
+          <button type="submit" className="submit-btn" disabled={btnDisabled}>
+            Submit
+          </button>
+        </form>
+        {message && <span className="alert-message">{message}</span>}
+      </div>
     </div>
   );
 };
