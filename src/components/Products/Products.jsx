0ec6bdd175de544bcb77240dd758ce5da896ed76
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-05-13 23:29:01 +0200

    add products.map into const productsInfo

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,22 +9,25 @@ const {getAllProducts, products} =useContext(ProductContext)
         getAllProducts()
     }, [])
 
+    const productsInfo = products.map((product) => {
+        return (
+            <div key={product.id}>
+                <img src={product.image} alt="imagen" />
+                <div>
+                    <h4>{product.name}</h4>
+                    
+                    <p>{product.description}</p>
+                    <span>{product.price}</span>
+                    <span>{product.Category.category}</span>
+                </div>
+            </div>
+        )
+    })
+
     return (
         <div>
-            {products.map((product) => {
-            return (
-                <div key={product.id}>
-                    <img src={product.image} alt="imagen" />
-                    <div>
-                        <h4>{product.name}</h4>
-                        
-                        <p>{product.description}</p>
-                        <span>{product.price}</span>
-                        <span>{product.Category.category}</span>
-                    </div>
-                </div>
-            )
-        })}</div>
+            {productsInfo}
+        </div>
     )
 }
 
