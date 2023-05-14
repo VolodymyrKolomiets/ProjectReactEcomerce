5de3a4d41fc1b8cff65de94498cbbd3c44c3e74d
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-05-14 19:29:42 +0200

    add function map in orders contex for extract productId and add some images to upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import './Footer.scss'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faGithub, faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { GithubOutlined, FacebookOutlined } from "@ant-design/icons";
 
 const Footer = () => {
   return (
+    <div className='footer-div'>
     <footer>
       <div className="footer-container">
         <h5>Created by: Volodymyr Kolomiiets</h5>
         <div className="social-icons">
-        <a href="https://github.com">
-          {/* <FontAwesomeIcon icon={faGithub} />  */}Facebook
+        <a href="https://github.com/VolodymyrKolomiets">
+        <GithubOutlined />
         </a>
         <a href="https://facebook.com">
-          {/* <FontAwesomeIcon icon={faFacebook} /> */}Github
+          <FacebookOutlined />
         </a>
         </div>
         <h5>Contact: kolomiietsv882gmail.com</h5>
       </div>
     </footer>
+    </div>
   );
 }
 
--- src/context/OrderContext/OrderState.jsx
+++ src/context/OrderContext/OrderState.jsx
@@ -8,11 +8,11 @@ const API_URL = "http://localhost:8080";
 export const OrdersContext = createContext();
 export const OrdersProvider = ({ children }) => {
 
-    const createOrder = async (order) => {
+    const createOrder = async () => {
         const token = JSON.parse(localStorage.getItem("token"));
-        // const orderId = cart.map(order => {cart})
+        const productIds = cart.map((product) => product.id);
         try {
-            await axios.post(API_URL + "/orders/createOrder", { productIds: order },
+            await axios.post(API_URL + "/orders/createOrder", { productIds },
                 {
                     headers: {
                         authorization: token,
